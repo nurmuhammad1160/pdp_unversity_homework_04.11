7af39d3d6eab9e16b5368545b78b9c424216b8bb
Nurmuhammad <nurmuhammadsamadov63@gmail.com>
2024-11-03 09:45:23 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,515 @@
+# # 1
+# yosh = int(input("Yoshingizni kiriting: "))
+
+# if yosh >= 18:
+#     print("voyaga yetga")
+# else:
+#     print("voyaga yetmaga")
+
+
+# 2
+# yosh = int(input("Yoshingizni kiriting: "))
+
+# if yosh == 5:
+#     print('alo')
+# elif yosh == 4:
+#     print('yahshi')
+# elif yosh == 3:
+#     print('qoniqarli')
+# elif yosh == 2:
+#     print("qoniqarsiz")
+# else:
+#     print("Bunday baho yo'q")
+
+
+# 3
+# oy = int(input("Oy raqamini kiriitng: "))
+
+# if (oy > 0 and oy <= 2) or oy == 12:
+#     print("Qish fasli")
+# elif oy >= 3 and oy <= 5:
+#     print("Bahor fasli")
+# elif oy >= 6 and oy <= 8:
+#     print("Yoz fasli")
+# elif oy >= 9 and oy <= 11:
+#     print("Kuz fasli")
+# else:
+#     print("Kechirasi bu raqamda oy mavjud emas")
+
+
+# 4
+# harorat = int(input("Havo haroratini kiriting: "))
+
+# if harorat >= 30:
+#     print("Havo issiq")
+# elif 30 < harorat > 20 or harorat >= 15:
+#     print("havo iliq")
+# elif harorat < 15:
+#     print("havo sovuq")
+# else:
+#     print('hatolik')
+
+
+# # 5
+# a_midat = int(input("Amal qilish mudatini kiriting: "))
+# kun = a_midat - 4
+# if kun > 5:
+#     print("hujjat amal qilish muddati tugadi")
+# else:
+#     print(f"{kun} kun qoldi tugashiga")
+
+# # 6
+# usul = input("To'lov usulini kiriting: ")
+
+# if usul == "naqt":
+#     savol = input("To'lov miqdorini kiriting: ")
+#     print(f"{savol} so'm")
+# elif usul == "karta":
+#     savol = input("Karta parolini kiriting: ")
+#     print(f"karta paroli {savol}")
+# else:
+#     print("Bunday to'lov usuli yo'q")
+
+
+# 7-masala 
+# frand=int(input("Enter number: "))
+
+# if 80<=frand and frand<=89:
+#     print("A")
+# elif 70 <=frand and frand<=79:
+#     print("B")
+# elif 60 <=frand and frand<=69:
+#     print("C")
+# elif 60 >frand :
+#     print("F")
+
+
+# 8-masala 
+# age=int(input("Enter your age: "))
+# if age>=18:
+#     print("Kattalar")
+# else:
+#     print("Bolalar")
+
+
+# 9-masala
+# wait=input("Ob havoni kiriting:")
+# if wait=="qor"or wait=="Qor" and wait=="yomg'ir" or wait=="Yonmg'ir":
+#     print("Kuylak kiyish tavsiya etilmaydi !!")
+# else:
+#     print("Kuylak kiyish tavsiya etildi !!")
+
+
+# 10-masala
+# card=input("Qaysi usulda tulov qilasz :")
+# if card=="Karta"  or card=="karta":
+#     print("Tulov uchun:",8600, 1235 ,2563 ,2589)
+# else:
+#     print("Tulov naqd berishingiz kerak !!")
+
+
+# 11-masala
+# cong=input("foizini kiriting:")
+# if cong>="70%":
+#     print("Sertifikat olishingiz mumkin ")
+# else:
+#     print("Sertifikat olishingiz mumkin emas")
+
+
+# 13-masala
+# num=int(input("Bahoni kiriting:"))
+# num1=int(input("Bahoni kiriting:"))
+# num2=int(input("Bahoni kiriting:"))
+# if num==5 and num1==5 and num2==5:
+#     print("A'lo")
+# elif num==5 or num==4  and num1==5 and num2==4:
+#     print("Yaxshi")
+# elif num==5 or num==4 and num1==4 or num2==5 or num2==4:
+#     print("Yaxshi") 
+# elif num==4 or num==3  and num1==3 or num2==3:
+#     print("Yomon")
+# 12-masala
+# num=int(input("Oyninig raqamini kiriting:"))
+# if num==1 or num==2 or num==12:
+#     print("Qish")
+# elif num==3 or num==4 or num==5:
+#     print("Bahor") 
+# elif num==6 or num==7 or num==8:
+#     print("Yoz")
+# elif num==9 or num==10 or num==11:
+#     print("Kuz")
+# else:
+#     print("[1:12] bulgan raqam kiritishingiz kerak !!")               
+
+
+# 14-masala
+# eat=input("Qaysi taomni aytasiz :")
+# print("Taom qilinyapti :",eat)
+
+
+# 15-masala
+# kilo=int(input("Kilometrni kiriting:"))
+# age=int(input("Moshinani yoshini kiriting:"))
+# if age>=10:
+#     print("Ehtiyot qismlarni tekshiring !!")
+# else:
+#     print("Moshinangiz hali kup xizmat qiladi !!")
+
+
+
+# 16-masala
+# age=int(input(" Yoshini kiriting:"))
+# jins=input("Jinsni kiriting:") 
+# if age>=10 and jins=="Erkak"  or jins=="erkak":
+#     print("Siz telefon sovg'a qilishingiz mukin")
+# elif age>=10 and jins=="Ayol" or jins=="ayol":
+#     print("Siz gul sovg'a qilishingiz mukin")    
+
+
+
+# 17-masala
+# reyt=int(input("Reytingni kiriting:"))
+# if reyt>=100:
+#     print("Siz reytingni yangilashiz kerek !!!")
+# else:
+#     print("Yangilashga hali bor !!!")
+
+
+# 18-masala
+# time=int(input("Vaqtni kiriting:"))
+# if 5<=time and time <=16:
+#     print("Hayrli kun")
+# else:
+#     print("Xayrli tun")
+
+
+
+# 19-masala
+# while True:
+#   passport=input("Parolingizni kiriting:")
+#   if len(passport)>=8:
+#     print("Parol muvaffaqiyatli qo'shildi")
+#     break
+#   else:
+#     print("Parol 8 ta harf bilan boshlanishi kerak")
+
+
+
+# 20 -masala
+# salary=int(input("Maoshingizni kiriting:"))
+# deposit=int(input("Qarz miqdorini kiriting:"))
+# if salary>deposit:
+#     print("Siz qarz berishingiz mumkin!!")
+# else:
+#     print("Sizga qarz berish mumkin emas!!")
+
+
+# 21-masala
+# yosh = int(input("Yoshingizni kiriting: "))
+
+# if 5 <= yosh <= 12:
+#     print("Sizga mos sport turlari: gimnastika, suzish, futbol, basketbol.")
+# elif 13 <= yosh <= 18:
+#     print("Sizga mos sport turlari: futbol, voleybol, yengil atletika, basketbol, jang san'ati.")
+# elif 19 <= yosh <= 40:
+#     print("Sizga mos sport turlari: yugurish, tennis, suzish, fitnes, yoga.")
+# elif yosh > 40:
+#     print("Sizga mos sport turlari: yoga, yurish, suzish, velosport.")
+# else:
+#     print("Kechirasiz, yosh noto‘g‘ri kiritilgan.")
+
+
+
+# 22-masala
+# vazn = float(input("Vazningizni kiriting (kg): "))
+# boy = float(input("Bo'yingizni kiriting (m): "))
+
+# bmi = vazn / (boy ** 2)
+
+# print(f"Sizning BMI ko'rsatkichingiz: {bmi:.2f}")
+
+# if bmi < 18.5:
+#     print("Siz ozg'insiz. Sog'lig'ingizni yaxshilash uchun ovqatlanishni muvozanatlang.")
+# elif 18.5 <= bmi < 24.9:
+#     print("Sizning vazningiz me'yorida.")
+# elif 25 <= bmi < 29.9:
+#     print("Sizda ortiqcha vazn bor. Sog'lom turmush tarzini olib borishga harakat qiling.")
+# else:
+#     print("Sizda semizlik mavjud. Sog'lig'ingizni yaxshilash uchun shifokorga murojaat qiling.")
+
+
+
+# 23-masala
+# reja=input("Reja kiriting:")
+# if len(reja)>0:
+#     print("Sayohatga chiqamziz !!")
+# else:
+#     print("Sayohatga chiqmaymiz !!")
+
+
+
+# 24-masala
+# count=int(input("Bajargan ishingizni sonni :"))
+# if count>=5:
+#     print("Yaxshi natija !!")
+# else:
+#     print("Yomon natija !!")
+
+
+
+
+# 25-masala
+# name=input("Kitob nomini kiriting:")
+# if len(name)>0:
+#     print("Kitob ugan man")
+# else:
+#     print("Kitob uqimagan man !")    
+
+
+# 26-masala
+# name=input("Komanda nomini kiriting:")
+# age=input("Natijani yozing:")
+# if  age=="Yutdi" or age=="yutdi":
+#     print("Tabriklaymiz!!")
+# else:
+#     print("Afsusdamiz!!")    
+
+
+
+
+
+
+
+
+
+#40
+# Bahoni Tekshirish: Foydalanuvchidan bahoni so'rang va bahoni A, B, C, D yoki F ga to'g'ri kelishini aniqlang.
+
+#baho = input("Iltimos, bahoni kiriting (A, B, C, D yoki F): ")
+
+#if baho.upper() == 'A':
+#  print("Sizning bahoningiz A.")
+#elif baho.upper() == 'B':
+#  print("Sizning bahoningiz B.")
+#elif baho.upper() == 'C':
+#  print("Sizning bahoningiz C.")
+#elif baho.upper() == 'D':
+#  print("Sizning bahoningiz D.")
+#elif baho.upper() == 'F':
+#  print("Sizning bahoningiz F.")
+#else:
+#  print("Iltimos, to'g'ri baho kiriting (A, B, C, D yoki F).")
+
+
+#41
+# #Mavsumni Aniqlash: Foydalanuvchidan oy raqamini so'rang va bu oy qishda, bahorda, yozda yoki kuzda ekanligini aniqlang.
+#raqam = int(input('oy raqamini kiriting[1-12]: '))
+#if 1<=raqam  and raqam <=2:
+#    print("Qish")
+#elif 3<=raqam and raqam<=6:
+#    print("Bahor")
+#elif 7<=raqam and raqam<=10:
+#    print("yoz") 
+#elif 8<=raqam and raqam <=12:
+#    print("kuz")   
+#else:
+#    print('bunday sonli oy raqami mavjud emas')
+
+
+#42
+#Ikki Sonni Taqqoslash: Foydalanuvchidan ikkita sonni so'rang va ularni qaysi biri katta, kichik yoki tengligini aniqlang.
+#son1 = int(input('birinchi sonni kiriting '))
+#son2 = int(input('ikkinchi sonni kiriting '))
+#if son1 > son2:
+#    print('son1 katta')
+#elif son1 < son2:
+#    print('son2 katta')
+#else:
+#    print('Bu sonlar teng')
+
+
+#43
+#Parolni Tekshirish: Foydalanuvchidan parolni so'rang va parol to'g'ri yoki noto'g'ri ekanligini aniqlang.
+
+#password1 = input('parolni kiriting : ')
+#password2 = input('parolni qaytadan kiriting : ')
+
+#if password1 == password2:
+#    print('parolni togri kiritdingiz')
+#else:
+#    print('parolni xato kiritdingiz')
+
+
+#44
+#Harfning Turini Aniqlash: Foydalanuvchidan bir harfni so'rang va bu katta yoki kichik harf ekanligini aniqlang.
+
+#harf = input("bitta harf kiriting: ")
+
+#if harf.isupper():
+#  print(f"{harf} katta harf.")
+#elif harf.islower():
+#  print(f"{harf} kichik harf.")
+#else:
+#  print(f"{harf} harf emas.")
+
+
+#45
+#Havo Haroratini Aniqlash: Foydalanuvchidan havo haroratini so'rang va u juda issiq, iliq, sovuq yoki juda sovuq ekanligini aniqlang.
+
+#temperature = input("haroratni kiriting[juda issiq/iliq/sovu/juda sovuq]: ")
+
+#if temperature == "juda issiq":
+#    print("havo harorat juda issiq")
+#elif temperature == "iliq":
+#    print("havo harorat iliq")
+#elif temperature == "sovuq":
+#    print("havo harorat sovuq")
+#elif temperature == "juda sovuq":
+#    print("havo harorat juda sovuq")
+#else:
+#    print("bunday havo harorati mavjud emas")
+
+
+#46
+#Mamlakatni Aniqlash: Foydalanuvchidan mamlakatini so'rang va agar u O'zbekiston bo'lsa, “Siz O'zbekistondasiz” deb yozing.
+
+#mamlakat = input("Iltimos, mamlakatingizni kiriting: ")
+
+#if mamlakat == "o'zbekiston" or mamlakat == "O'zbekiston":
+#  print("Siz O'zbekistondasiz.")
+#else:
+#  print(f"Siz {mamlakat}da yashaysiz.")
+
+
+
+#47
+#Balandlikni Aniqlash: Foydalanuvchidan balandligini so'rang va u qisqa, o'rtacha yoki baland ekanligini aniqlang.
+
+#height = input("balandlikni kiriitng [qisqa/ o'rtacha / baland]: ")
+
+#if height == "qisqa":
+#    print("balandlik qisqa")
+#elif height == "o'rtacha":
+#    print("balandlik o'rtacha")
+#elif height == "baland":
+#    print("balandlik baland")
+#else:
+#    print("bunaqa balanlik berilmagan")
+
+
+#48
+#Kitob Narxini Tekshirish: Foydalanuvchidan kitob narxini so'rang va narxni arzon, o'rtacha yoki qimmat ekanligini aniqlang.
+
+#summa = input("kitob narxini kiriting [arzon / o'tacha / qimmat]: ")
+
+#if summa == "arzon":
+#    print(f'kitob narxi {summa} ekan')
+#elif summa == "o'rtacha":
+#    print(f'kitob narxi {summa} ekan')
+#elif summa == "qimmat":
+#    print(f'kitob narxi {summa} ekan')
+#else:
+#    print("bunaqa summada kitob mavjud emas")
+
+
+#49
+#Sport Darajasini Aniqlash: Foydalanuvchidan sport darajasini so'rang va u yangi, o'rta yoki tajribali sportchi ekanligini aniqlang.
+
+#degree = input("sportchining darajasini kiriting ")
+
+#if degree == "yangi":
+#    print(f'sportchining darjasi {degree} ')
+#elif degree == "o'rta":
+#    print(f'sportchining darjasi {degree} ')
+#elif degree == "tajribali":
+#    print(f'sportchining darjasi {degree} ')
+#else:
+#    print("bunaqa darjadagi sportchi mavjud emas")
+
+
+#50
+#Sog'liqni Tekshirish: Foydalanuvchidan belgilari haqida ma'lumot oling va sog'liq holatini aniqlang (masalan, isitma, yo'tal).
+
+#health = input("sog'lig'ingiz haqida malumot kiriting : ")
+
+#if health == "isitma":
+#    print(f"sizning {health}ingiz bor")
+#elif health == "yotal":
+#    print(f"sizning {health}ingiz bor")
+#else:
+#    print("siz soppa sog'siz")
+
+
+#51
+#Kreditni Tasdiqlash: Foydalanuvchidan kredit balini so'rang va u kredit olish uchun mos kelishini aniqlang.
+
+#kredit_bali = int(input("kredit balingizni kiriting: "))
+
+#if kredit_bali >= 1000:
+#  print("Siz kredit olish uchun mossiz!")
+#else:
+#  print("Siz kredit olish uchun mos emassiz.")
+
+#52
+#Kafedagi Buyurtma: Foydalanuvchidan kafeda buyurtma (kofe, choy, sharbat) so'rang va buyurtma tayyorlanishini xabar bering.
+
+#buyurtma = input("buyurtmangizni kiriting: ")
+
+#if buyurtma == 'kofe':
+#    print(f'sizning buyurtmangiz yani {buyurtma} hoz tayyor boladi')
+#elif buyurtma == 'choy':
+#    print(f'sizning buyurtmangiz yani {buyurtma} hoz tayyor boladi')
+#elif buyurtma == 'sharbat':
+#    print(f'sizning buyurtmangiz yani {buyurtma} hoz tayyor boladi')
+#else:
+#    print("bizda bunaqa buyurtma mavjude emas!")
+
+
+#53
+#Tezlikni Aniqlash: Foydalanuvchidan avtomobil tezligini so'rang va u tezlikni oshirdimi yoki normal ekanligini aniqlang.
+
+#tezlik = int(input("Iltimos, avtomobil tezligini kiriting: "))
+#maksimal_tezlik = 250
+
+#if tezlik > maksimal_tezlik:
+#  print("Avtomobil tezlikni oshirdi.")
+#else:
+#  print("Avtomobil tezligi normal.")
+
+
+#54
+#Tatilni Aniqlash: Foydalanuvchidan ta'til vaqtini so'rang va yoz yoki qish ta'tili ekanligini aniqlang.
+
+#oy = input("ta'til oyi (raqam sifatida)ni kiriting: ")
+
+#if oy in ['6', '7', '8']:
+#  print("Bu yoz ta'tili.")
+#elif oy in ['12', '1', '2']:
+#  print("Bu qish ta'tili.")
+#else:
+#  print("Bu ta'til yoz yoki qish emas.")
+
+#55
+#Maktab Bahosi: Foydalanuvchidan maktab bahosini so'rang va u yuqori, o'rtacha yoki past baho ekanligini aniqlang.
+
+#baho = input("bahoni kiriting : ")
+
+#if baho == "5":
+#    print("sizning bahoingiz yuqori")
+#elif baho == "4":
+#    print("sizning bahoingiz o'rtacha")
+#elif baho == "2":
+#    print("sizning bahoingiz past")
+#else:
+#    print("bunaqa baho mavjude emas")
+
+
+#56
+#Raqamni Aniqlash: Foydalanuvchidan raqam kiritishni so'rang va kiritilgan qiymat raqam ekanligini aniqlang.
+
+# raqam = input("raqam kiriting: ")
+
+# if raqam.isdigit():
+#   print(f"{raqam} raqam.")
+# else:
+#   print(f"{raqam} raqam emas.")
